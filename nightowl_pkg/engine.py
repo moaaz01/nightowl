7fac117ae08c8a5dd919ee9407c2666e38113a9c
moaaz01 <moaaz01@users.noreply.github.com>
2026-08-24 02:36:17 +0300

    chore: bump version to 8.3.2

--- nightowl_pkg/engine.py
+++ nightowl_pkg/engine.py
@@ -16,7 +16,7 @@ Commands:
   nightowl proxy              Network proxy setup
 """
 
-__version__ = "8.3.1"
+__version__ = "8.3.2"
 
 import os, sys, json, re, zipfile, hashlib, argparse, shutil, warnings
 from pathlib import Path
